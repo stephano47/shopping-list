b8000d75a21164a7a02922613c8a7c4b1cd156ab
stephano47 <stephanomelv@gmail.com>
2024-09-26 08:56:54 -0400

    test 2

---
+++ src/main/java/ShoppingList.java
@@ -0,0 +1,20 @@
+public class ShoppingList {
+
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Shopping List");
+        System.out.println("______________");
+        System.out.println("Apples");
+        System.out.println("Oranges");
+        System.out.println("Bananas");
+        System.out.println("Blueberries");
+        System.out.println("Blackberries");
+        System.out.println("Chicken");
+        System.out.println("Pears");
+        System.out.println("Pastries");
+        System.out.println("Cereal");
+        System.out.println("Cheese");
+    }
+}
